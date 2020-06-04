7bbfddf45341797ede4bd60cc47f6832524b824b
Andres Acevedo - mran3 <x88andres@hotmail.com>
2020-06-04 14:31:12 -0500

    Initial Commit

---
+++ Axrt/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Axrt
+//
+//  Created by troquer on 6/4/20.
+//  Copyright © 2020 zourz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
